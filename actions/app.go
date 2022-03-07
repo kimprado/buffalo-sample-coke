60966060e85653e618325677ac99de269c9b52e8
Kim Prado <kimprado21@gmail.com>
2022-03-07 07:24:22 -0300

    Add Widgets Resource

--- actions/app.go
+++ actions/app.go
@@ -65,6 +65,7 @@ func App() *buffalo.App {
 
 		app.GET("/", HomeHandler)
 
+		app.Resource("/widgets", WidgetsResource{})
 		app.ServeFiles("/", http.FS(public.FS())) // serve files from the public directory
 	}
 
